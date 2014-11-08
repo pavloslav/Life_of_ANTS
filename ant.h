ffc2fd6db8fed48509611e4169e3f52eb1ed6052
StoBF <Bipycok@gmail.com>
2014-11-08 08:16:46 +0200

    Create ant.h

---
+++ ant.h
@@ -0,0 +1,44 @@
+#include <GL/freeglut.h>
+
+#ifndef ANT_H
+#define ANT_H
+
+#define WIDTH 300
+#define HEIGHT 150
+#define BARR 50
+
+class Ant
+{
+private:
+    double Speed;
+    double Live;
+    double SpeedAtack;
+
+public:
+    int size;
+    bool turn;
+    bool Invent;
+    int StartPosX;
+    int StartPosY;
+    int StartSize;
+    Ant();
+    void set_speed(double);
+    double get_speed();
+    void set_live(double);
+    double get_live();
+    void set_speedatack(double);
+    double get_speedatack();
+    int direction;
+    void go_home(int i);
+    void init();
+    void DrawAnt(int i);
+    void search(int j);
+    void navig(double* targ, int *ptr , int i);
+    void move();
+    void go();
+    void eject(int i);
+    void eat(int i);
+    void chek(int i);
+
+};
+#endif // ANT_H
