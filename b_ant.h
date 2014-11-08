4f6359752f38f5eeaf7613b8fcc4f69eefef0660
StoBF <Bipycok@gmail.com>
2014-11-08 08:17:41 +0200

    Create b_ant.h

---
+++ b_ant.h
@@ -0,0 +1,27 @@
+
+
+#ifndef B_ANT_H
+#define B_ANT_H
+#include "ant.h"
+
+class B_ant: public Ant
+{
+public:
+    int direction;
+    int size;
+    bool turn;
+    bool Invent;
+    B_ant();
+    void DrawSnake(int i);
+    void move(int i);
+     void go_home(int i);
+    void navig(double* targ, int *ptr , int i);
+        void search(int j);
+        void chek(int i);
+        void eat(int i);
+    void cut();
+    void eject(int i);
+
+};
+
+#endif // B_ANT_H
