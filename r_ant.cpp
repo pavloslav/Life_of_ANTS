e6ad23a30c1408f12965d84016f9de70c3787e3f
StoBF <Bipycok@gmail.com>
2014-11-08 08:24:14 +0200

    Create r_ant.cpp

---
+++ r_ant.cpp
@@ -0,0 +1,45 @@
+#include "r_ant.h"
+
+
+
+
+R_ant::R_ant()
+{
+    Invent = false;
+    turn = false;
+    size = Ant::StartSize;
+    direction = GLUT_KEY_RIGHT;
+    //set_speed(25);
+}
+
+/*
+void R_ant::search(int j)
+{
+        double size = 20;
+        double targ[20];
+        //double bufer=0;
+
+        double delta=0;
+        int deltaX;
+        int deltaY;
+
+        int* n=new int;
+        *n=0;
+        for (int i=0;i<size;++i){     //перераховує масив m[i]
+            deltaX=r_ant[j].x-m[i]->x;
+            deltaY=r_ant[j].y-m[i]->y;
+            delta=((deltaX*deltaX)+(deltaY*deltaY));
+
+            targ[i] = sqrt(delta);
+            if(targ[i]<=targ[*n]){
+                *n=i;
+            }
+
+
+
+        }
+        navig(targ,n,j );
+}
+
+
+*/
